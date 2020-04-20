e1d72b835c580bfa1d0eccdb8846741faaff7fb0
李張誌 <kasoarcat@gmail.com>
2020-04-20 14:11:23 +0800

    d

--- main.py
+++ main.py
@@ -38,7 +38,6 @@ if platform.system() == 'Linux':
 
     def install(package):
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-        
     install('install/pycocotools-2.0-cp36-cp36m-linux_x86_64.whl')
     install('install/pytoan-0.6.4-py3-none-any.whl')
     install('install/imgaug-0.2.6-py3-none-any.whl')
